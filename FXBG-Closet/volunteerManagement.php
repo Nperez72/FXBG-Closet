2403d98c63a32e09770a81a72b3a8d6ca60f9b1b
Dang <dangphung4@gmail.com>
2025-10-15 12:36:26 -0400

    style: remove hardcoded styles from volunteerManagement.php to improve maintainability and prepare for CSS variable implementation

--- FXBG-Closet/volunteerManagement.php
+++ FXBG-Closet/volunteerManagement.php
@@ -34,22 +34,6 @@
 $tailwind_mode = true;
 require_once('header.php');
 ?>
-<style>
-        .date-box {
-            background: #274471;
-            padding: 7px 30px;
-            border-radius: 50px;
-            box-shadow: -4px 4px 4px rgba(0, 0, 0, 0.25) inset;
-            color: white;
-            font-size: 24px;
-            font-weight: 700;
-            text-align: center;
-        }
-	.dropdown {
-	    padding-right: 50px;
-	}
-
-</style>
 <!-- BANDAID END, REMOVE ONCE SOME GENIUS FIXES -->
 
 </head>
