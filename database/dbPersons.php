bb9220c9c11a09e864e4987fc075bd30e67745c3
Bethanie05 <behackett@outlook.com>
2025-10-27 09:43:34 -0400

    Ensure selected role change is stored somewhere for access by index.php.

--- database/dbPersons.php
+++ database/dbPersons.php
@@ -151,7 +151,7 @@ function updated_retrieve_person($id) { // (username! not id)
         return false;
     }
     $result_row = mysqli_fetch_assoc($result);
-    var_dump($result_row);
+    //var_dump($result_row);
     $thePerson = make_a_person($result_row);
 //    mysqli_close($con);
     return $thePerson;
