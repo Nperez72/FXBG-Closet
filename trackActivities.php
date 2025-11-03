792aba885ee169302a236ea296fe861f587d8633
Colin Chu <cchu@mail.umw.edu>
2025-11-03 15:00:47 -0500

    cherry pick old commit to fix photo upload

--- trackActivities.php
+++ trackActivities.php
@@ -80,74 +80,50 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // were photos uploaded?
     if (isset($_FILES["activity_images"]) && !$errors) {
         // try to filter out non-images
-        $allowed = array("jpg" => "image/jpeg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
+        $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
         // loop based on how many images
         for ($x = 0; $x < count($_FILES["activity_images"]["name"]); $x++) {
-            if (is_uploaded_file($_FILES["activity_images"]["tmp_name"][$x])) {
-                $fname = basename($_FILES["activity_images"]["name"][$x]);
-                $fname = strtolower($fname);
-                $ftemp = $_FILES["activity_images"]["tmp_name"][$x];
-                $ftype = mime_content_type($ftemp);
-                $fsize = $_FILES["activity_images"]["size"][$x];
+            $fname = basename($_FILES["activity_images"]["name"][$x]);
+            $fname = strtolower($fname);
+            $ftype = $_FILES["activity_images"]["type"][$x];
+            $ftemp = $_FILES["activity_images"]["tmp_name"][$x];
+            $ext = pathinfo($fname, PATHINFO_EXTENSION);
 
+        // only allow the above file extensions
+        // TODO security could be better
+            if (!array_key_exists($ext, $allowed)) {
+                $errors = true;
+                $popupMessage = 'Invalid photo file type.';
+                $popupType = 'error';
+                break;
+            }
 
-                // only allow the above MIME types
-            // TODO security could be better
-
-                if (in_array($ftype, $allowed)) {
-                    // does it already exist?
-                    if (file_exists("uploads/" . $person_id . "_" . $event_id . "_" . $date . "_" . $fname)) {
-                        $errors = true;
-                        $popupMessage = $fname . " already exists.";
-                        break;
-                    } else {
-                        // max image size in bytes is 10MB
-                        $maxsize = 10 * 1024 * 1024;
-                        $destination = "uploads/" . $person_id . "_" . $event_id . "_" . $date . "_" . $fname . ".jpeg";
-                        // temporary file destination
-                        $image = null;
-                // enforce file size
-                        if ($fsize > $maxsize) {
-                            $showPopup = true;
-                            $popupMessage = 'Failed to upload photo. Max size is 10MB.';
-                            $popupType = 'error';
-                            break;
-                        }
-                // compress images larger than 5MB
-                        if ($fsize > $maxsize / 2) {
-                            // tmp imagecreate
-                                $image = match ($ftype) {
-                                    "image/jpeg" => imagecreatefromjpeg($ftemp),
-                                    "image/gif" => imagecreatefromgif($ftemp),
-                                    "image/png" => imagecreatefrompng($ftemp),
-                                };
-                    //compress
-                        }
-                        // move it to the uploads folder.
-                        // format: person_id_event_id_date_fname
-                        if (imagejpeg($image, $destination)) {
-                            $fresult = add_media(null, $event_id, basename($fname), $ftype, $ext, $activity_description, basename($ftemp), $date);
-                            if (!$fresult) {
-                                $showPopup = true;
-                                $popupMessage = 'Failed to upload photo. Please try again.';
-                                $popupType = 'error';
-                                break;
-                            }
-                        } else {
-                            $errors = true;
-                            $showPopup = true;
-                            $popupMessage = 'Failed to upload file: ' . htmlspecialchars($fname) . '. Please try again.';
-                            $popupType = 'error';
-                            break;
-                        }
-                    }
-                } else {
+        // only allow the above MIME types
+        // TODO security could be better
+            if (in_array($ftype, $allowed)) {
+                // does it already exist?
+                if (file_exists("uploads/" . $person_id . "_" . $event_id . "_" . $date . "_" . $fname)) {
                     $errors = true;
-                    // something went wrong with the photo.
-                    $popupMessage = "Error: " . $_FILES["activity_images"]["name"][$x];
-                    $popupType = 'error';
+                    $popupMessage = $fname . " already exists.";
                     break;
+                } else {
+                    // move it to the uploads folder.
+                    // format: person_id_event_id_date_fname
+                    move_uploaded_file($ftemp, "uploads/" . $person_id . "_" . $event_id . "_" . $date . "_" . $fname);
+                    $fresult = add_media(null, $event_id, basename($fname), $ftype, $ext, $activity_description, basename($ftemp), $date);
+                    if (!$fresult) {
+                        $showPopup = true;
+                        $popupMessage = 'Failed to log activity. Please try again.';
+                        $popupType = 'error';
+                        break;
+                    }
                 }
+            } else {
+                $errors = true;
+                // something went wrong with the photo.
+                $popupMessage = "Error: " . $_FILES["activity_images"]["error"][$x];
+                $popupType = 'error';
+                break;
             }
         }
     }
