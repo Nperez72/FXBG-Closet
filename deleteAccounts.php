478efc5e515f8068e4ec30c70b04f46222cbebc0
Joey Ryerson <joseph.a.ryerson@gmail.com>
2025-11-17 15:17:07 -0500

    Purging SPCA references
    Changed reference to SPCA to pride

--- deleteAccounts.php
+++ deleteAccounts.php
@@ -69,7 +69,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <head>
         <?php require_once('universal.inc') ?>
         <script src="https://cdn.tailwindcss.com"></script>
-        <title>Fredericksburg SPCA | Change Password</title>
+        <title>Fredericksburg Pride | Delete Account</title>
     </head>
     <body>
         <?php require_once('header.php') ?>
